3f4125aa6ea7f8112ea9e39605b61337aba91270
nghiapd17012003 <45303883+nghiapd17012003@users.noreply.github.com>
2023-09-16 21:39:47 +1200

    First commit

---
+++ backup.py
@@ -0,0 +1,70 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from datetime import datetime
+import os
+import shutil
+
+gauth = GoogleAuth()
+# Try to load saved client credentials
+# Still need verifcation for the first time. Ignore the warning in cmd.
+
+# Uncomment the next line if you have your credential token detail save in a text file of your local machine
+gauth.LoadCredentialsFile("/var/www/html/itflow/creds.txt") #change the parameter with your path to the text file have credential token
+if gauth.credentials is None:
+    # Authenticate if they're not there
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    # Refresh them if expired
+    gauth.Refresh()
+else:
+    # Initialize the saved creds
+    gauth.Authorize()
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("/var/www/html/itflow/creds.txt") #change the parameter with your path to where you want to save the credential token (basically same as line 12)
+drive = GoogleDrive(gauth)
+
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+directory_path = '/var/lib/mysql/itflow'  #path of the folder you want to upload
+destination_path = '~/Downloads'  #zip location
+archive_name = 'db'
+
+os.chmod('/var/lib/mysql/itflow', 0o777)
+
+# Provide the format 'zip' when calling make_archive
+shutil.make_archive(
+    os.path.join(destination_path, archive_name),
+    'zip',  # Specify the archive format
+    directory_path
+)
+
+os.chmod('~/Downloads/db.zip', 0o777)
+
+file = drive.CreateFile({'title': 'DB - ' + dt_string + '.zip', 'parents': [{'id': '1XBNScG51PFP3bYOUbJ9uZCBsh_o6sU5B'}]}) #id is the last part of your google drive url 
+file.SetContentFile('~/Downloads/db.zip')
+file.Upload()
+
+
+
+
+#The second folder, pretty much the same!
+directory_path2 = '/var/www/html/itflow/uploads/expenses'  # receipts folder path
+destination_path2 = '~/Downloads'  # db zip location
+archive_name2 = 'receipts'
+
+os.chmod('/var/www/html/itflow/uploads/expenses', 0o777)
+
+# Provide the format 'zip' when calling make_archive
+shutil.make_archive(
+    os.path.join(destination_path2, archive_name2),
+    'zip',  # Specify the archive format
+    directory_path2
+)
+
+os.chmod('~/Downloads/receipts.zip', 0o777)
+
+file2 = drive.CreateFile({'title': 'receipts - ' + dt_string + '.zip', 'parents': [{'id': '1XBNScG51PFP3bYOUbJ9uZCBsh_o6sU5B'}]})
+file2.SetContentFile('~/Downloads/receipts.zip')
+file2.Upload()
+
